f947abaa9c2637222d75f50f95583613b1d7918c
MateusMacedo <mateusouza2014@live.com>
2022-08-11 08:34:36 -0300

    feat: Adicionando Parametros DTO V2

--- Decorator Patterns/Decorator.Infra/Stores/V2/CarStoreV2.cs
+++ Decorator Patterns/Decorator.Infra/Stores/V2/CarStoreV2.cs
@@ -1,6 +1,7 @@
 ﻿using Bogus;
 using Decorator.Application.Interfaces;
 using Decorator.Application.Models.V2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,11 +14,17 @@ namespace Decorator.Infra.Stores.V2
         public CarStoreV2()
         {
             var id = 0;
+            var data = DateTime.Now;
+
             var testOrdersV2 = new Faker<CarV2>()
+            .RuleFor(o => o.DataSimulacao, f => data)
             .RuleFor(o => o.IdSimulacao, f => ++id)
             .RuleFor(o => o.YearVeiculo, f => f.Random.Int(1950, 2020))
             .RuleFor(o => o.BrandVeiculo, f => f.Vehicle.Manufacturer())
-            .RuleFor(o => o.ModelVeiculo, f => f.Vehicle.Model());
+            .RuleFor(o => o.ModelVeiculo, f => f.Vehicle.Model())
+            .RuleFor(o => o.TipoVeiculo, f => f.Vehicle.Type())
+            .RuleFor(o => o.Company, f => f.Company.CompanyName())
+            .RuleFor(o => o.CompanySuffix, f => f.Company.CompanySuffix());
             CarStorageV2 = testOrdersV2.Generate(10);
         }
 
